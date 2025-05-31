42b22d05993eaef1fdaf904114303e25b386971c
fiftythanks <mikhail.sholokhov@tutamail.com>
2025-05-31 19:07:44 +0700

    chore: update file structure for React projects

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import Header from '@layout/Header';
+import Main from '@layout/Main';
+import Footer from '@layout/Footer';
 
-/**
- *
- */
-function App() {
-  return <h1>Hello, React + TypeScript + Webpack!</h1>;
+export default function App() {
+  return (
+    <>
+      <Header />
+      <Main />
+      <Footer />
+    </>
+  );
 }
-
-export default App;
